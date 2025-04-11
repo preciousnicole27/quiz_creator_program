fcf0938f6c450be8343d0b0dd5683586736bb03f
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-11 14:27:48 +0800

    added a pseudocode

---
+++ quiz_creator_program.py
@@ -0,0 +1,25 @@
+# Main program
+# import libraries
+# initialize color formatting
+# set filename for storing questions along with the existing questions
+# define the function that will check files
+# check if quiz file exists
+# define function that will let user add questions
+# use loop
+# ask user to input the correct answer for the given question
+# save the question given along with the answer
+# ask user if they want to add another question
+# define the function that will read the questions in the existing text file
+# store 
+# return 
+# define the function that will view the questions added 
+# print each question number and text
+# define the function that will allow user to remove questions
+# write the updated list after
+# define the function that will allow user to take the quiz
+# shuffle/ randomize the questions
+# ask for user input (answers)
+# print score with percentage
+# define the main menu function
+# loop through four options
+# let user choose which they want to do
