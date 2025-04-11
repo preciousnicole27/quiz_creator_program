5130cfff28db4080a2a8f91d9e21ed18d1026cc1
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-04-11 17:24:37 +0800

    define the major functions that will be used in the program

--- quiz_creator_program.py
+++ quiz_creator_program.py
@@ -5,27 +5,33 @@ import random
 from colorama import init, Fore, Style
 # initialize color formatting
 init(autoreset=True)
-filename= "quiz_questions.txt"
 # set filename for storing questions along with the existing questions
+filename= "quiz_questions.txt"
 # define the function that will check files
+def check_file():
 # check if quiz file exists
 # define function that will let user add questions
+def add_question():
 # use loop
 # ask user to input the correct answer for the given question
 # save the question given along with the answer
 # ask user if they want to add another question
 # define the function that will read the questions in the existing text file
+def quiz_program(filename):
 # store 
 # return 
 # define the function that will view the questions added 
+def view_questions():
 # print each question number and text
 # define the function that will allow user to remove questions
+def remove_question():
 # write the updated list after
 # define the function that will allow user to take the quiz
+def start_quiz(questions, num_questions = 5):
 # shuffle/ randomize the questions
 # ask for user input (answers)
 # print score with percentage
 # define the main menu function
-
+def main():
 # loop through four options
 # let user choose which they want to do
