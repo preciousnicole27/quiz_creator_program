6a1ffdfa8be9ffe50f87bd8805cd05f0525a7d80
preciousnicole27 <preciousnicoledelacruz.27@gmail.com>
2025-05-09 18:16:58 +0800

    added a pseudocode

---
+++ quiz_game.py
@@ -0,0 +1,15 @@
+# Quiz Game Program
+# import libraries
+# init color format auto reset every print
+# define file for quiz questions
+# define the function that will check if the quiz file exists
+# if not found return false
+# define the function that will load the questions
+    # if file doesnt exist return an empty list
+# return
+# define the funnction that will randomize questions
+# display score
+# define the function of the main menu
+# use loop to show main menu 
+# print menu options
+# call main function to begin the program
